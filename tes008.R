6a6b161e29a1dcb13411357055216a59b8bd3f59
Kristo Nova <vista.indonesia@gmail.com>
2020-03-06 17:29:25 +0700

    add collatz conjecture coding in R

---
+++ tes008.R
@@ -0,0 +1,43 @@
+library(sna)
+library(igraph)
+library(textclean)
+library(tidyverse)
+
+num_r <- read.dot("collatz.dot")
+g.graph <- graph.adjacency(num_r)
+
+angka <- V(g.graph)$name
+angka <- gsub(";", "", angka)
+angka <- replace_white(angka)
+angka <- as.numeric(angka)
+angka <- data_frame(angka)
+angka <- angka %>% 
+  filter(!duplicated(angka))
+
+kamus <- data_frame(k1 = seq_along(1:1048576))
+
+kamus1 <- kamus %>%
+  mutate(komplit_1 = (18*k1)+4, # 1st complete condition
+         komplit_2 = (18*k1)-2, # 2st complete condition
+  )
+
+hasil_1 <- kamus1 %>%
+  filter(komplit_1 %in% angka$angka) %>% 
+  select(angka = komplit_1)
+
+hasil_2 <- kamus1 %>%
+  filter(komplit_2 %in% angka$angka) %>% 
+  select(angka = komplit_2)
+
+# gabungan nodes yang komplit
+komplit <- bind_rows(hasil_1, hasil_2)
+komplit <- komplit %>% 
+  filter(!duplicated(angka)) %>% 
+  mutate(ket = "komplit")
+
+# nodes yang tidak komplit
+tdk_komplit <- angka %>% 
+  filter(!angka %in% komplit$angka) %>% 
+  mutate(ket = "tidak komplit")
+
+hasil_total <- bind_rows(komplit, tdk_komplit)
